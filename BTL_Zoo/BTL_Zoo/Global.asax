b0cd4a0ba029e534c911ea70f4b6b1e63450d03c
Shindo <bachngochieph2@gmail.com>
2016-11-11 17:02:53 +0700

    khởi tạo layout, CSDL

---
+++ BTL_Zoo/BTL_Zoo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BTL_Zoo.MvcApplication" Language="C#" %>
---
+++ BTL_Zoo/BTL_Zoo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BTL_Zoo.Startup))]
+namespace BTL_Zoo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

6c6c0cabce90c6b4f6e8b49d82225a2efe9e339e
Shindo <bachngochieph2@gmail.com>
2016-11-14 06:37:36 +0700

    version 3
    dần hoàn thiện home

--- BTL_Zoo/BTL_Zoo/Controllers/HomeController.cs
+++ BTL_Zoo/BTL_Zoo/Controllers/HomeController.cs
@@ -3,16 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using BTL_Zoo.Entities;
 namespace BTL_Zoo.Controllers
 {
     public class HomeController : Controller
     {
         //
         // GET: /Home/
+        Entities.Zoo mod = new Entities.Zoo();
         public ActionResult Index()
         {
-            return View();
+            ListZoo list = new ListZoo();
+            list.LsDongVat = mod.DongVats.ToList();
+            list.LsSuKien = mod.SuKiens.ToList();
+            return View(list);
         }
+       
 	}
 }
